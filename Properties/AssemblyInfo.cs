de4cd0b1085f85bf360d5cee57a79937178ee9c3
SeongJoon Kwak (SJ) <sjkwak@microsoft.com>
2016-09-23 07:52:41 +0000

    [Fix build error - Microsoft.Azure.Relay.csproj - Duplicate System.Reflection.AssemblyDescription attribute] Merge remote-tracking branch 'origin/sjkwak.fixBuildBreak'

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,9 +5,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCulture("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c1746881-1ba9-4eb6-9507-c849cfe04113")]
