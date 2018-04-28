7fc617462f6a337712fee54324e6c04e918e3b1e
dlstucki <dstucki@microsoft.com>
2018-04-28 07:56:11 -0700

    Use HttpUtility in .net framework, QueryHelpers in .net core (#68)

--- src/Microsoft.Azure.Relay/HybridConnectionUtility.cs
+++ src/Microsoft.Azure.Relay/HybridConnectionUtility.cs
@@ -4,13 +4,17 @@
 namespace Microsoft.Azure.Relay
 {
     using System;
+    using System.Collections.Generic;
     using System.Collections.Specialized;
-    using System.IO;
     using System.Net;
-    using System.Net.WebSockets;
     using System.Text;
-    using System.Threading;
-    using System.Threading.Tasks;
+#if NET45
+    using System.Web;
+#else
+    // NETSTANDARD
+    using Microsoft.AspNetCore.WebUtilities;
+    using Microsoft.Extensions.Primitives;
+#endif
 
     static class HybridConnectionUtility
     {
@@ -49,6 +53,18 @@ namespace Microsoft.Azure.Relay
             }.Uri;
         }
 
+#if NET45
+        public static NameValueCollection ParseQueryString(string queryString)
+        {
+            return HttpUtility.ParseQueryString(queryString);
+        }
+#else
+        public static IDictionary<string, StringValues> ParseQueryString(string queryString)
+        {
+            return QueryHelpers.ParseQuery(queryString);
+        }
+#endif
+
         /// <summary>
         /// Filters out any query string values which start with the 'sb-hc-' prefix.  The returned string never
         /// has a '?' character at the start.
@@ -61,34 +77,11 @@ namespace Microsoft.Azure.Relay
             }
 
             queryString = queryString.TrimStart(QuestionMark);
-            var queryStringCollection = new NameValueCollection(StringComparer.OrdinalIgnoreCase);
-            foreach (var nameValueString in queryString.Split(Ampersand))
-            {
-                if (!string.IsNullOrEmpty(nameValueString))
-                {
-                    string[] nameAndValue = nameValueString.Split(EqualSign, 2);
-                    if (nameAndValue.Length == 2)
-                    {
-                        queryStringCollection[nameAndValue[0]] = nameAndValue[1];
-                    }
-                    else
-                    {
-                        queryStringCollection[nameAndValue[0]] = string.Empty;
-                    }
-                }
-            }
+            var queryStringCollection = ParseQueryString(queryString);
 
-            return FilterQueryString(queryStringCollection);
-        }
-
-        /// <summary>
-        /// Filters out any query string values which start with the 'sb-hc-' prefix.  The returned string never
-        /// has a '?' character at the start.
-        /// </summary>
-        public static string FilterQueryString(NameValueCollection queryString)
-        {
             var sb = new StringBuilder(256);
-            foreach (string key in queryString.Keys)
+
+            foreach (string key in queryStringCollection.Keys)
             {
                 if (key != null && key.StartsWith(HybridConnectionConstants.QueryStringKeyPrefix, StringComparison.Ordinal))
                 {
@@ -100,7 +93,7 @@ namespace Microsoft.Azure.Relay
                     sb.Append('&');
                 }
 
-                sb.Append(WebUtility.UrlEncode(key)).Append('=').Append(WebUtility.UrlEncode(queryString[key]));
+                sb.Append(WebUtility.UrlEncode(key)).Append('=').Append(WebUtility.UrlEncode(queryStringCollection[key]));
             }
 
             return sb.ToString();
