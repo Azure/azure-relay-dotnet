a47af210cc2fdb256ddd763a2b710fc9617322ff
bailiu <bailiu@microsoft.com>
2021-07-13 18:04:23 -0700

    Implementations to support RBAC for Hybrid Connections

---
+++ src/Microsoft.Azure.Relay/Common/AzureActiveDirectoryTokenProvider.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Azure.Relay
+{
+    using System;
+    using System.Threading.Tasks;
+
+    class AzureActiveDirectoryTokenProvider : TokenProvider
+    {
+        readonly string authority;
+        readonly object authCallbackState;
+        readonly AuthenticationCallback authCallback;
+
+        internal AzureActiveDirectoryTokenProvider(AuthenticationCallback authenticationCallback, string authority, object state)
+        {
+            this.authCallback = authenticationCallback ?? throw new ArgumentNullException(nameof(authenticationCallback));
+            this.authority = authority;
+            this.authCallbackState = state;
+        }
+
+        /// <summary>
+        /// Gets a <see cref="SecurityToken"/> for the given audience and duration.
+        /// </summary>
+        /// <param name="audience">The resource URI for which the token is authorized. For example: http://contoso.servicebus.windows.net/my-hybridconnection</param>
+        /// <param name="validFor">The time span that specifies the timeout value for the message that gets the security token</param>
+        /// <returns><see cref="SecurityToken"/></returns>
+        protected override async Task<SecurityToken> OnGetTokenAsync(string audience, TimeSpan validFor)
+        {
+            var tokenString = await this.authCallback(TokenProvider.AadRelayAudience, this.authority, this.authCallbackState).ConfigureAwait(false);
+            return new JsonSecurityToken(tokenString, audience);
+        }
+    }
+}
--- src/Microsoft.Azure.Relay/Common/TokenProvider.cs
+++ src/Microsoft.Azure.Relay/Common/TokenProvider.cs
@@ -6,14 +6,25 @@ namespace Microsoft.Azure.Relay
     using System;
     using System.Text;
     using System.Threading.Tasks;
+    using global::Azure.Identity;
 
     /// <summary>
     /// This abstract base class can be extended to implement additional token providers.
     /// </summary>
     public abstract class TokenProvider
     {
+        internal const string AadRelayAudience = "https://relay.azure.net/";
         internal static readonly TimeSpan DefaultTokenTimeout = TimeSpan.FromMinutes(60);
         internal static readonly Func<string, byte[]> MessagingTokenProviderKeyEncoder = Encoding.UTF8.GetBytes;
+
+        /// <summary>
+        /// A user supplied handler that would be invoked to obtain the AAD access token string.
+        /// </summary>
+        /// <param name="audience">The AAD resource URI which the access token is authorized. For example: https://relay.azure.net/</param>
+        /// <param name="authority">Address of the authority to issue the AAD token.</param>
+        /// <param name="state">State to be delivered to the callback.</param>
+        /// <returns></returns>
+        public delegate Task<string> AuthenticationCallback(string audience, string authority, object state);
 
         /// <summary>Initializes a new instance of the <see cref="TokenProvider" /> class. </summary>
         protected TokenProvider()
@@ -47,6 +58,39 @@ namespace Microsoft.Azure.Relay
             return new SharedAccessSignatureTokenProvider(keyName, sharedAccessKey);
         }
 
+        /// <summary>Creates an Azure Active Directory token provider.</summary>
+        /// <param name="authCallback">A user supplied handler that would be invoked to obtain the AAD access token string.</param>
+        /// <param name="authority">Address of the authority to issue the AAD token. For example, https://login.microsoftonline.com/{TenantId}</param>
+        /// <param name="state">State to be delivered to callback.</param>
+        /// <returns>The <see cref="Microsoft.Azure.Relay.TokenProvider" /> for returning Json web token.</returns>
+        public static TokenProvider CreateAzureActiveDirectoryTokenProvider(
+            AuthenticationCallback authCallback,
+            string authority,
+            object state = null)
+        {
+            Fx.Assert(authCallback != null, $"{nameof(authCallback)} cannot be null.");
+            Fx.Assert(authority != null, $"{nameof(authority)} cannot be null.");
+            return new AzureActiveDirectoryTokenProvider(authCallback, authority, state);
+        }
+
+        /// <summary>
+        /// Creates a TokenProvider for a Azure managed identity with a default instance of <see cref="ManagedIdentityCredential"/>.
+        /// </summary>
+        /// <returns>The <see cref="TokenProvider" /> for returning Json web token.</returns>
+        public static TokenProvider CreateManagedIdentityTokenProvider()
+        {
+            return new ManagedIdentityTokenProvider();
+        }
+
+        /// <summary>
+        /// Creates a TokenProvider for a Azure managed or user-assigned identity with a provided instance of <see cref="ManagedIdentityCredential"/>.
+        /// </summary>
+        /// <returns>The <see cref="TokenProvider" /> for returning Json web token.</returns>
+        public static TokenProvider CreateManagedIdentityTokenProvider(ManagedIdentityCredential managedIdentityCredential)
+        {
+            return new ManagedIdentityTokenProvider(managedIdentityCredential);
+        }
+
         /// <summary>
         /// Gets a <see cref="SecurityToken"/> for the given audience and duration.
         /// </summary>
@@ -66,7 +110,7 @@ namespace Microsoft.Azure.Relay
         }
 
         /// <summary>
-        /// Implemented by derived TokenProvider types to generate their SecurityTokens.
+        /// Implemented by derived TokenProvider types to generate their <see cref="SecurityToken"/>.
         /// </summary>
         /// <param name="audience">The target audience for the security token.</param>
         /// <param name="validFor">How long the generated token should be valid for.</param>
