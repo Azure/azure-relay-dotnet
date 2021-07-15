01aacac68bff85a8d22fc4165018ffbe14887af1
David Stucki <dstucki@microsoft.com>
2021-07-15 10:50:59 -0700

    Update some pipeline/build files

--- package.cmd
+++ package.cmd
@@ -2,7 +2,7 @@
 PUSHD "%~dp0"
 
 rem CDP_PACKAGE_VERSION_SEMANTIC set by internal 
-if not defined CDP_PACKAGE_VERSION_SEMANTIC set CDP_PACKAGE_VERSION_SEMANTIC=2.0.1.0-dev
+if not defined CDP_PACKAGE_VERSION_SEMANTIC set CDP_PACKAGE_VERSION_SEMANTIC=3.0.0-preview
 
 if defined TF_BUILD (
     set BuildConfiguration=Release
