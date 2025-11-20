b05c2af8b732b7b4a54128b8acfd1b92a098d52b
Jacob <jacobjordan@microsoft.com>
2025-11-20 16:02:04 -0500

    version update

--- package.cmd
+++ package.cmd
@@ -2,7 +2,7 @@
 PUSHD "%~dp0"
 
 rem CDP_PACKAGE_VERSION_SEMANTIC set by internal 
-if not defined CDP_PACKAGE_VERSION_SEMANTIC set CDP_PACKAGE_VERSION_SEMANTIC=3.0.1
+if not defined CDP_PACKAGE_VERSION_SEMANTIC set CDP_PACKAGE_VERSION_SEMANTIC=3.1.0
 
 if defined TF_BUILD (
     set BuildConfiguration=Release
